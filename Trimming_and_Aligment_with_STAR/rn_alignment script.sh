1a894e31713c67248166e393f57b95a4d3fe1899
Arman Simonyan <51209290+rmnsimonyan@users.noreply.github.com>
2021-06-12 00:12:22 +0200

    Update rn_alignment script.sh

--- Trimming_and_Aligment_with_STAR/rn_alignment script.sh
+++ Trimming_and_Aligment_with_STAR/rn_alignment script.sh
@@ -1,12 +1,13 @@
 r1=()
 r2=()
+dir=$(pwd)
 
-for i in $(find /ngc/people/armsim/gm/people/armsim/Downsample/1m/data/trim2 -name "*R1_val_1_val_1.fq" | sort)
+for i in $(find /ngc/people/armsim/gm/people/armsim/Downsample/10k/data/trim2 -name "*R1_val_1_val_1.fq" | sort)
 do
         r1+=($i)
 done
 
-for i in $(find /ngc/people/armsim/gm/people/armsim/Downsample/1m/data/trim2 -name "*R2_val_2_val_2.fq" | sort)
+for i in $(find /ngc/people/armsim/gm/people/armsim/Downsample/10k/data/trim2 -name "*R2_val_2_val_2.fq" | sort)
 do
         r2+=($i)
 done
@@ -14,9 +15,12 @@ done
 
 for i in $(seq 0 $((${#r1[@]}-1)))
 do
+        cd $dir
         folder=$(echo ${r1[$i]} | cut -f12 -d "/")
         mkdir $folder
         cp STARmapHuman.sh $folder
-        sh $folder/STARmapHuman.sh ${r1[i]} ${r2[i]}
+        cd $folder
+        #echo $folder/STARmapHuman.sh ${r1[i]} ${r2[i]}
+        sh $( echo STARmapHuman.sh ${r1[i]} ${r2[i]})
 
 done
