14f7c734cf67d726b5f049e50accad36b3d64f03
Arman Simonyan <51209290+rmnsimonyan@users.noreply.github.com>
2021-06-11 22:06:19 +0200

    Update STARalign.sh

--- Trimming_and_Aligment_with_STAR/STARalign.sh
+++ Trimming_and_Aligment_with_STAR/STARalign.sh
@@ -7,17 +7,17 @@ module load gcc/8.2.0 star/2.7.2b
 
 fwd=$1
 rev=$2
-OUTDIR=$2
+#OUTDIR=$3
 
 echo $fwd
-#echo $rev
+echo $rev
 
-rm -rf "$OUTDIR/*_STARtmp"
+#rm -rf "$OUTDIR/*_STARtmp"
 
 echo "commandline:"
 #cmd="STAR --runThreadN 10 --genomeDir /ngc/projects/gm/people/vicpro/cambridge/scripts/ref/STAR_ERCC --sjdbGTFfile /ngc/projects/gm/people/vicpro/cambridge/scripts/ref/Homo_sapiens_GRCh38_dna_sm_primary_assembly_ERCC_fa.gtf --readFilesIn $fwd --genomeLoad NoSharedMemory --quantMode TranscriptomeSAM GeneCounts --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate --limitBAMsortRAM 35000000000"
 
-cmd="STAR --runThreadN 10 --genomeDir /ngc/shared/resources/h.sapiens/hg19/genomes/1000genomes/20110707/star/2.7.2b --sjdbGTFfile /ngc/projects/gm/data/resources/ngs/hg19/annotation/GRCh37_latest_genomic.gtf --readFilesIn $fwd --genomeLoad NoSharedMemory --quantMode TranscriptomeSAM GeneCounts --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate --limitBAMsortRAM 35000000000"
+cmd="STAR --runThreadN 10 --genomeDir /ngc/shared/resources/h.sapiens/hg19/genomes/1000genomes/20110707/star/2.7.2b --sjdbGTFfile /ngc/projects/gm/data/resources/ngs/hg19/annotation/GRCh37_latest_genomic.gtf --readFilesIn $fwd $rev --genomeLoad NoSharedMemory --quantMode TranscriptomeSAM GeneCounts --readFilesCommand cat --outSAMtype BAM SortedByCoordinate --limitBAMsortRAM 35000000000"
 
 echo "$cmd"
 eval "$cmd"
