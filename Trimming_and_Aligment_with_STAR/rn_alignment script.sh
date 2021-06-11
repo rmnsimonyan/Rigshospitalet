122c9cb7aca9a60db5e6a808c612f7e47174ebd0
Arman Simonyan <51209290+rmnsimonyan@users.noreply.github.com>
2021-06-11 22:56:10 +0200

    Update rn_alignment script.sh

--- Trimming_and_Aligment_with_STAR/rn_alignment script.sh
+++ Trimming_and_Aligment_with_STAR/rn_alignment script.sh
@@ -1,6 +1,30 @@
-for i in $(find ../1m/data/trim2 -name "*R1_val_1_val_1.fq" | sort)
+r1=()
+r2=()
+
+for i in $(find /ngc/people/armsim/gm/people/armsim/Downsample/1m/data/trim2 -name "*R1_val_1_val_1.fq" | sort)
 do
-	echo $i
+        r1+=($i)
 done
 
-find ../1m/data/trim2 -name "*R1_val_1_val_1.fq" | sort
+for i in $(find /ngc/people/armsim/gm/people/armsim/Downsample/1m/data/trim2 -name "*R2_val_2_val_2.fq" | sort)
+do
+        r2+=($i)
+done
+
+
+ for i in $(seq 1 {#r1[@]})
+ do
+ 	echo $i-1
+ done
+
+echo
+
+for i in $(find /ngc/people/armsim/gm/people/armsim/Downsample/1m/data/trim2 -name "*R2_val_2_val_2.fq" | sort)
+do
+        r2+=($i)
+done
+
+
+
+
+echo ${a[@]}
